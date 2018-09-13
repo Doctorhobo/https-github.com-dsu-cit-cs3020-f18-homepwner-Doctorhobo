49e26a3ae002bd940da0c8a8ef4375249acff4f6
Blake Cook <bcook@cit-scc113-1138.cs.dixie.edu>
2018-09-13 09:16:29 -0600

    Initial Commit

---
+++ homepwnerTests/homepwnerTests.swift
@@ -0,0 +1,36 @@
+//
+//  homepwnerTests.swift
+//  homepwnerTests
+//
+//  Created by Blake Cook on 9/13/18.
+//  Copyright © 2018 Blake Cook. All rights reserved.
+//
+
+import XCTest
+@testable import homepwner
+
+class homepwnerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
